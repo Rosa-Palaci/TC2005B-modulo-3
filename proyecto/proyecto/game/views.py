04f91f8834917569fd94fd17c53ffb649814ff82
Rosa Palaci <palacibelrose431@gmail.com>
2023-02-21 11:42:51 -0600

    def de suma y se modifico el def de division

--- proyecto/proyecto/game/views.py
+++ proyecto/proyecto/game/views.py
@@ -9,6 +9,7 @@ class Fraccion:
         self.den = den
     def toJSON(self):
         return dumps(self, default=lambda o:o.__dict__, sort_keys=False, indent=4)
+    
 # Create your views here.
 def index(request):
     #return HttpResponse('<h1> Hola desde Django</h1>')
@@ -18,6 +19,21 @@ def proceso(request):
     nombre = request.POST['nombre']
     nombre = nombre.upper()
     return HttpResponse('Hola '+ nombre)
+
+@csrf_exempt
+def suma(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+    num1 = body['numerador1']
+    den1 = body['denominador1']
+    num2 = body['numerador2']
+    den2 = body['denominador2']
+    num_resultado = num1 + num2
+    den_resultado = den1 + den2
+    resultado = Fraccion(num_resultado,den_resultado)
+    resultado_json = resultado.toJSON()
+    return HttpResponse(resultado_json, content_type = "text/json-comment-filtered")
+
 
 def bienvenida(request):
     letrero = "Bienvenida"
@@ -35,8 +51,11 @@ def division(request):
     body = loads(body_unicode)
     a = body['a']
     b = body['b']
+    resultado = Fraccion(a,b)
     #a = request.POST['a']
     #b = request.POST['b']
     #c = int(a)/int(b)
-    return HttpResponse("La division de "+str(a)+" "+str(b))
+    json_resultado = Fraccion.toJSON()
+    return HttpResponse(json_resultado, \
+        content_type= "text/json-comment-filtered")
     #return HttpResponse("Division") + str(c)
